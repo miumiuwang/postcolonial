225851031d34b82d38e32030c9a90675513e4aae
miumiuwang <124361690+miumiuwang@users.noreply.github.com>
2023-11-21 17:06:18 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function showDetails() {
+  document.getElementById("details").style.display = "block";
+}
+
+function hideDetails() {
+  document.getElementById("details").style.display = "none";
+}
